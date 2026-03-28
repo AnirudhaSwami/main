6221aa0e045f5225d2b7c50b09ae8db180050ae3
Anirudha <aniswami1996@gmail.com>
2026-03-28 18:48:35 +0530

    Add java practice files J001-J010 and ignore .class files.

---
+++ Java/J002_Output.java
@@ -0,0 +1,16 @@
+/*
+    Challenge: Print Name and Age
+    Test your understanding of Java Output by completing a small coding challenge.
+
+    Instructions
+    Inside main(), complete the following steps:
+    Use print() or println() to print your name
+    Use another print() or println() to print your age
+*/
+
+public class J002_Output {
+    public static void main(String [] args){
+        System.out.println("Anirudha Swami");
+        System.out.println(30);
+    }
+}
---
+++ Java/J008_WhileLoop.java
@@ -0,0 +1,23 @@
+/*
+
+Challenge: Count from 1 to 5
+Test your understanding of Java While Loops by completing a small coding challenge.
+
+Instructions
+Inside main(), complete the following steps:
+Declare an int variable named i and set it to 1
+Use a while loop that runs as long as i <= 5
+Inside the loop, print the value of i
+Increase i by 1 in each loop
+
+*/
+
+public class J008_WhileLoop {
+    public static void main(String [] args){
+       int i = 1;
+       while(i <= 5){
+        System.out.println(i);
+        i++;
+       } 
+    }    
+}
